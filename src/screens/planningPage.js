7768cca37376e4c45e1f8bfd36327c7f5b395b7c
Amsomeboys <khnun1090@gmail.com>
2023-08-19 04:08:22 +0700

    Add Image

--- src/screens/planningPage.js
+++ src/screens/planningPage.js
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import SaveButton from '../components/saveButton';
+import Picture from '../../assets/image/friend.jpg';
 
 const styles = StyleSheet.create({
   header: {
@@ -18,18 +19,20 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   textContainer: {
-    height: 60,
+    height: 'auto',
     width: '100%',
     shadowOpacity: 1,
     shadowOffset: 1,
     borderColor: '#151515',
     backgroundColor: '#fff',
     borderRadius: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   title: {
     fontSize: 20,
   },
-  text: { fontSize: 18, color: '#3D3D3D' },
+  text: { fontSize: 18, color: '#3D3D3D', marginTop: 40 },
 });
 
 const PlanningPage = ({ route, navigation }) => {
@@ -44,7 +47,19 @@ const PlanningPage = ({ route, navigation }) => {
           {dayjs().format('DD/MM/YYYY')}
         </Text>
         <View style={styles.textContainer}>
+          <Image
+            source={{
+              uri: 'https://i.pinimg.com/originals/c5/5d/31/c55d3169242a69c0a4de9fc74a9bfa0b.jpg',
+            }}
+            style={{ width: 100, height: 100 }}
+            /* Internet Source */
+          ></Image>
           <Text style={[styles.text, { margin: 20 }]}>{text}</Text>
+          <Image
+            source={Picture}
+            style={{ height: 100, width: 100 }}
+            /* Local Source */
+          />
         </View>
         <View>
           <SaveButton
